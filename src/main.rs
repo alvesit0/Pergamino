a43493acfc237458db8f26775224f0696394a87f
alvesit0 <adrianamorales900@gmail.com>
2025-12-20 13:52:28 +0100

    hello wordl

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,51 @@
-fn main() {
-    println!("Hello, world!");
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
+
+use eframe::egui;
+
+fn main() -> eframe::Result {
+	env_logger::init();
+	let options = eframe::NativeOptions {
+		viewport: egui::ViewportBuilder::default().with_inner_size([320.0, 240.0]),
+		..Default::default()
+	};
+
+	eframe::run_native(
+        "My egui App",
+        options,
+        Box::new(|cc| {
+            // This gives us image support:
+            egui_extras::install_image_loaders(&cc.egui_ctx);
+
+            Ok(Box::<MyApp>::default())
+        }),
+    )
+}
+
+#[derive(Default)]
+struct MyApp {
+    name: String,
+    age: u32,
+}
+
+// impl Default for MyApp {
+// 	fn default() -> Self {
+//         Self {
+//             name: "Arthur".to_owned(),
+//             age: 42,
+//         }
+//     }
+// }
+
+impl MyApp {
+    fn new(cc: &eframe::CreationContext<'_>) -> Self {
+        Self::default()
+    }
+}
+
+impl eframe::App for MyApp {
+	fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
+		egui::CentralPanel::default().show(ctx, |ui| {
+			ui.heading("Hello world!");
+		});
+	}
 }
